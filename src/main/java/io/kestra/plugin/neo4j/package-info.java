aba0e8f6d7de920fdfa719ee6d3a2f8f267db7e4
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:34:52 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/neo4j/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using the Neo4j graph database.",
+    categories = PluginSubGroup.PluginCategory.DATABASE
+)
+package io.kestra.plugin.neo4j;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
