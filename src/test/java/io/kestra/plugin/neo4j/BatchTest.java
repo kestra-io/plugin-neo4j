c829e54b0d304287d1bce1ebd7b981112d32f103
Ludovic DEHON <tchiot.ludo@gmail.com>
2025-05-20 09:58:59 +0200

    chore(test): inject main tenants on storage and runOne

--- src/test/java/io/kestra/plugin/neo4j/BatchTest.java
+++ src/test/java/io/kestra/plugin/neo4j/BatchTest.java
@@ -7,6 +7,7 @@ import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.serializers.FileSerde;
 import io.kestra.core.storages.StorageInterface;
+import io.kestra.core.tenant.TenantService;
 import io.kestra.core.utils.IdUtils;
 import io.kestra.core.utils.TestsUtils;
 import jakarta.inject.Inject;
@@ -78,6 +79,6 @@ public class BatchTest {
             n1.put("position", UUID.randomUUID().toString());
             FileSerde.write(output, n1);
         }
-        return storageInterface.put(null, null, URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
+        return storageInterface.put(TenantService.MAIN_TENANT, null, URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
     }
 }
