b89ec107a65ca238d1ea3b605566ec407ac9a71e
ben8t <pimpaudben@gmail.com>
2025-12-30 15:40:25 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/neo4j/package-info.java
+++ src/main/java/io/kestra/plugin/neo4j/package-info.java
@@ -1,6 +1,9 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using the Neo4j graph database.",
-    categories = PluginSubGroup.PluginCategory.DATABASE
+    categories = PluginSubGroup.PluginCategory.DATABASE,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA
+    }
 )
 package io.kestra.plugin.neo4j;
 
